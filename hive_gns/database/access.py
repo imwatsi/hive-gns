a13249eeaf976d56e297db0b52069680b7fd6e11
imwatsi <imwatsi@gmail.com>
2022-04-17 16:53:08 +0400

    improve db access script

--- hive_gns/database/access.py
+++ hive_gns/database/access.py
@@ -1,37 +1,38 @@
 from hive_gns.database.core import DbSession
-from hive_gns.tools import populate_by_schema
+from hive_gns.tools import populate_by_schema, normalize_types
 
-read_db = DbSession()
-write_db = DbSession()
+_read_db = DbSession()
+_write_db = DbSession()
 
 def select(sql:str, schema:list):
-    _res = read_db.execute(sql,None)
+    _res = _read_db.select(sql)
     res = []
     if _res:
-        assert len(schema) == len(res[0])
-        for x in res:
+        assert len(schema) == len(_res[0])
+        for x in _res:
             res.append(populate_by_schema(x,schema))
-        return res
+        return normalize_types(res)
 
-def insert(sql:str):
-    pass
-
-def delete(sql:str):
-    pass
-
-def update(sql:str):
+def write(sql:str):
     try:
-        write_db.execute(sql, None)
+        _write_db.execute(sql, None)
+        _write_db.commit()
         return True
     except:
         return False
 
 def perform(func:str, params:list):
-    for p in params:
-        assert isinstance(p, str), 'function params must be strings'
-    _params = ", ".join(params)
+    #for p in params:
+        #assert isinstance(p, str), 'function params must be strings'
+    string_params = ["%s" for p in params]
+    parameters = ", ".join(string_params)
     try:
-        write_db.execute(f"PERFORM gns.{func}( {params} );", None)
+        _write_db.execute(f"SELECT {func}( {parameters} );", params)
+        _write_db.commit()
         return True
     except:
         return False
+
+def alter_schema(sql:str):
+    _write_db.execute(sql)
+    _write_db.commit()
