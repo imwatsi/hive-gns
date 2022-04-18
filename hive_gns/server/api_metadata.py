e388e100c5a0116733c2291067c82d6096cb491e
imwatsi <imwatsi@gmail.com>
2022-04-18 21:13:23 +0400

    fastapi metadata

---
+++ hive_gns/server/api_metadata.py
@@ -0,0 +1,39 @@
+"""Metadata for the FastAPI instance"""
+
+# Main
+
+TITLE = "Hive Global Notification System (API)"
+
+DESCRIPTION = """
+    Global notification system for dApps on the Hive Blockchain.
+"""
+
+VERSION = "1.0"
+
+CONTACT = {
+    "name": "imwatsi",
+    "url": "https://hive.blog/@imwatsi",
+    "email": "imwatsi@outlook.com",
+}
+
+LICENSE = {
+    "name": "MIT License"
+}
+
+
+# Tags for Plugs
+
+TAGS_METADATA = [
+    {
+        "name": "system",
+        "description": "System endpoints"
+    },
+    {
+        "name": "core",
+        "description": "Access core notifications",
+    },
+    {
+        "name": "splinterlands",
+        "description": "Splinterlands notifications",
+    }
+]
