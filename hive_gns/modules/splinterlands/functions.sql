586a9f838bd6f1adcd5f41c4d794f7f6722c9327
imwatsi <imwatsi@gmail.com>
2022-04-25 20:36:52 +0400

    add error handling to function

--- hive_gns/modules/splinterlands/functions.sql
+++ hive_gns/modules/splinterlands/functions.sql
@@ -19,10 +19,10 @@ CREATE OR REPLACE FUNCTION gns.sm_token_transfer( _gns_op_id BIGINT, _trx_id CHA
                 -- normal transfer_operation
                 _req_auths := ARRAY(SELECT json_array_elements_text((_body->'value'->'required_auths')));
                 _from := _req_auths[1];
-                _to := _body->'value'->'json'->>'to';
-                _qty := _body->'value'->'json'->'qty';
-                _memo := _body->'value'->'json'->>'memo';
-                _token := _body->'value'->'json'->>'token';
+                _to := (_body->'value'->>'json')::json->>'to';
+                _qty := (_body->'value'->>'json')::json->>'qty';
+                _memo := (_body->'value'->>'json')::json->>'memo';
+                _token := (_body->'value'->>'json')::json->>'token';
 
                 _remark := FORMAT('you have received %s %s from %s', _qty, _token, _from);
 
@@ -35,5 +35,9 @@ CREATE OR REPLACE FUNCTION gns.sm_token_transfer( _gns_op_id BIGINT, _trx_id CHA
                 INSERT INTO gns.account_notifs (gns_op_id, trx_id, account, module_name, notif_name, created, remark, payload)
                 VALUES (_gns_op_id, _trx_id, _to, 'splinterlands', _notif_name, _created, _remark, _body);
             END IF;
+        EXCEPTION WHEN OTHERS THEN
+                RAISE NOTICE E'Got exception:
+                SQLSTATE: % 
+                SQLERRM: %', SQLSTATE, SQLERRM;
         END;
         $function$;
