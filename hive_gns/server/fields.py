e2acc3b8374990c3efb9957bacf56d651adbd4be
imwatsi <imwatsi@gmail.com>
2022-05-06 11:36:49 +0400

    add fields for prefs endpoint

--- hive_gns/server/fields.py
+++ hive_gns/server/fields.py
@@ -4,12 +4,18 @@ class Fields:
         """Core module SQL fields."""
         @classmethod
         def get_transfers(cls, extra=None):
-            """Field for the `_get_transfers()` core endpoint function."""
+            """Fields for the `_get_transfers()` core endpoint function."""
             res = ['created', 'remark']
             if extra:
                 return res.extend(extra)
             else:
                 return res
+        
+        @classmethod
+        def get_preferences(cls):
+            """Fields for the `_get_preferences()` core endpoint function."""
+            res = ['prefs', 'prefs_updated']
+            return res
     
     class Splinterlands:
         """Splinterlands module's SQL fields."""
