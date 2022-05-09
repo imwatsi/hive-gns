4663510671d35b427d223af0d4c7c1dd632c27d0
imwatsi <imwatsi@gmail.com>
2022-05-09 14:56:08 +0400

    accounts router and endpoints

---
+++ hive_gns/server/core/accounts.py
@@ -0,0 +1,83 @@
+"""Top level account endpoints."""
+from fastapi import APIRouter, HTTPException
+from hive_gns.database.access import select
+from hive_gns.server.fields import Fields
+
+from hive_gns.server.system_status import get_module_list
+from hive_gns.tools import is_valid_hive_account
+
+router_core_accounts = APIRouter()
+
+def _get_all_notifs(acc, limit=None, op_data=False):
+    if op_data:
+        fields = Fields.Global.get_all_notifs(['payload'])
+    else:
+        fields = Fields.Global.get_all_notifs()
+    _fields = ", ".join(fields)
+    sql = f"""
+        SELECT {_fields}
+        FROM gns.account_notifs
+        WHERE account = '{acc}'
+        AND created > (
+            SELECT (last_reads->>'all')::timestamp
+            FROM gns.accounts WHERE account = '{acc}'
+        )
+    """
+    if limit:
+        sql += f"LIMIT {limit}"
+    res = select(sql, fields)
+    return res
+
+def _get_unread_count(acc):
+    sql = f"""
+        SELECT COUNT(*)
+        FROM gns.account_notifs
+        WHERE account = '{acc}'
+        AND created > (
+            SELECT (last_reads->>'all')::timestamp
+            FROM gns.accounts WHERE account = '{acc}'
+        );
+    """
+    res = select(sql, ['count'], True)
+    return res['count']
+
+def _get_preferences(account, module=None):
+    fields = Fields.Core.get_preferences()
+    _fields = ", ".join(fields)
+    sql = f"""
+        SELECT {_fields} FROM gns.accounts
+        WHERE account = '{account}';
+    """
+    res = select(sql, fields, True)
+    if module and module in res:
+        return res[module]
+    return res
+
+@router_core_accounts.get("/api/{account}/preferences", tags=['accounts'])
+def account_preferences(account:str, module:str = None):
+    if module and module not in get_module_list():
+        raise HTTPException(status_code=400, detail="the module is not valid or is unavailable at the moment")
+    if '@' not in account:
+        raise HTTPException(status_code=400, detail="missing '@' in account")
+    if not is_valid_hive_account(account.replace('@', '')):
+        raise HTTPException(status_code=400, detail="invalid Hive account entered for 'account'")
+    prefs = _get_preferences(account, module)
+    return prefs or {}
+
+@router_core_accounts.get("/api/{account}/notifications", tags=['accounts'])
+async def account_notifications(account:str, limit:int=100, op_data:bool=False):
+    if '@' not in account:
+        raise HTTPException(status_code=400, detail="missing '@' in account")
+    if not is_valid_hive_account(account.replace('@', '')):
+        raise HTTPException(status_code=400, detail="invalid Hive account entered for")
+    notifs = _get_all_notifs(account.replace('@', ''), limit, op_data)
+    return notifs or []
+
+@router_core_accounts.get("/api/{account}/unread", tags=['accounts'])
+async def account_unread_count(account:str):
+    if '@' not in account:
+        raise HTTPException(status_code=400, detail="missing '@' in account")
+    if not is_valid_hive_account(account.replace('@', '')):
+        raise HTTPException(status_code=400, detail="invalid Hive account entered for")
+    count = _get_unread_count(account.replace('@', ''))
+    return count
