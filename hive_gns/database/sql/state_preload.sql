ada9e077baf0c06bd5e775f167ec5a34a3c3cfd6
imwatsi <imwatsi@gmail.com>
2022-06-29 19:15:20 +0400

    update state preload

--- hive_gns/database/sql/state_preload.sql
+++ hive_gns/database/sql/state_preload.sql
@@ -10,7 +10,7 @@ CREATE OR REPLACE FUNCTION gns.load_state( _first_block BIGINT, _last_block BIGI
             _hive_op_type_id SMALLINT;
             _transaction_id VARCHAR(40);
             _body JSON;
-            _hash VARCHAR;
+            _hash BYTEA;
             _new_id BIGINT;
         BEGIN
             FOR temprow IN
@@ -20,27 +20,28 @@ CREATE OR REPLACE FUNCTION gns.load_state( _first_block BIGINT, _last_block BIGI
                     gnsov.block_num,
                     gnsov.timestamp,
                     gnsov.trx_in_block,
-                    gnsov.body
+                    gnsov.body,
+                    gnstv.trx_hash
                 FROM hive.gns_operations_view gnsov
+                LEFT JOIN hive.gns_transactions_view gnstv
+                    ON gnstv.block_num = gnsov.block_num
+                    AND gnstv.trx_in_block = gnsov.trx_in_block
                 WHERE gnsov.block_num >= _first_block
                     AND gnsov.block_num <= _last_block
-                    AND ppov.op_type_id = 18
-                    AND ppov.body::json->'value'->>'id' = 'gns'
+                    AND gnsov.op_type_id = 18
+                    AND gnsov.body::json->'value'->>'id' = 'gns'
                 ORDER BY gnsov.block_num, trx_in_block, gnsov.id
             LOOP
                 _hive_opid := temprow.hive_opid;
                 _block_num := temprow.block_num;
                 _block_timestamp = temprow.timestamp;
-                _hash := (
-                    SELECT gnstv.trx_hash FROM hive.gns_transactions_view gnstv
-                    WHERE gnstv.block_num = temprow.block_num
-                    AND gnstv.trx_in_block = temprow.trx_in_block);
-                _transaction_id := encode(_hash::bytea, 'escape');
+                _hash := temprow.trx_hash;
                 _hive_op_type_id := temprow.op_type_id;
                 _body := (temprow.body)::json;
 
-                PERFORM gns.core_gns(0, '0000000000000000000000000000000000000000', _block_timestamp, _body, 'gns');
+                PERFORM gns.core_gns(0, encode('\x0000000000000000000000000000000000000000','hex'), _block_timestamp, _body, 'gns');
 
             END LOOP;
+            UPDATE gns.global_props SET state_preloaded = true;
         END;
     $function$;
